96cf7aa57b491752018eab5b99a0ca4c1fa4f703
zuo <zuo_zxl@163.com>
2020-06-18 18:26:58 +0800

    添加地址类

---
+++ src/main/java/common/Address.java
@@ -0,0 +1,112 @@
+package common;
+
+import java.sql.Timestamp;
+
+public class Address {
+	private int addressId;
+	private int userId;
+	private String receiverName;
+	private String receiverPhone;
+	private String receiverMobile;
+	private String receiverProvince;
+	private String receiverCity;
+	private String receiverDistrict;
+	private String receiverAddress;
+	private String receiverZip;
+	private Timestamp createTime;
+	private Timestamp updateTime;
+	public Address() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Address(int addressId, int userId, String receiverName, String receiverPhone, String receiverMobile,
+			String receiverProvince, String receiverCity, String receiverDistrict, String receiverAddress,
+			String receiverZip, Timestamp createTime, Timestamp updateTime) {
+		super();
+		this.addressId = addressId;
+		this.userId = userId;
+		this.receiverName = receiverName;
+		this.receiverPhone = receiverPhone;
+		this.receiverMobile = receiverMobile;
+		this.receiverProvince = receiverProvince;
+		this.receiverCity = receiverCity;
+		this.receiverDistrict = receiverDistrict;
+		this.receiverAddress = receiverAddress;
+		this.receiverZip = receiverZip;
+		this.createTime = createTime;
+		this.updateTime = updateTime;
+	}
+	public int getAddressId() {
+		return addressId;
+	}
+	public void setAddressId(int addressId) {
+		this.addressId = addressId;
+	}
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getReceiverName() {
+		return receiverName;
+	}
+	public void setReceiverName(String receiverName) {
+		this.receiverName = receiverName;
+	}
+	public String getReceiverPhone() {
+		return receiverPhone;
+	}
+	public void setReceiverPhone(String receiverPhone) {
+		this.receiverPhone = receiverPhone;
+	}
+	public String getReceiverMobile() {
+		return receiverMobile;
+	}
+	public void setReceiverMobile(String receiverMobile) {
+		this.receiverMobile = receiverMobile;
+	}
+	public String getReceiverProvince() {
+		return receiverProvince;
+	}
+	public void setReceiverProvince(String receiverProvince) {
+		this.receiverProvince = receiverProvince;
+	}
+	public String getReceiverCity() {
+		return receiverCity;
+	}
+	public void setReceiverCity(String receiverCity) {
+		this.receiverCity = receiverCity;
+	}
+	public String getReceiverDistrict() {
+		return receiverDistrict;
+	}
+	public void setReceiverDistrict(String receiverDistrict) {
+		this.receiverDistrict = receiverDistrict;
+	}
+	public String getReceiverAddress() {
+		return receiverAddress;
+	}
+	public void setReceiverAddress(String receiverAddress) {
+		this.receiverAddress = receiverAddress;
+	}
+	public String getReceiverZip() {
+		return receiverZip;
+	}
+	public void setReceiverZip(String receiverZip) {
+		this.receiverZip = receiverZip;
+	}
+	public Timestamp getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(Timestamp createTime) {
+		this.createTime = createTime;
+	}
+	public Timestamp getUpdateTime() {
+		return updateTime;
+	}
+	public void setUpdateTime(Timestamp updateTime) {
+		this.updateTime = updateTime;
+	}
+	
+}
