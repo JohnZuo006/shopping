2bc717f505f97997a20b59a4089a458fa8cb81c4
zuo <zuo_zxl@163.com>
2020-06-22 20:00:54 +0800

    建立文件上传servlet

---
+++ src/main/java/servlet/UploadServlet.java
@@ -0,0 +1,77 @@
+package servlet;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.UUID;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
+/**
+ * Servlet implementation class UploadServlet
+ */
+@WebServlet("/Upload")
+public class UploadServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UploadServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String path="D:/ftpfile/";
+		String fn=null;
+        
+        DiskFileItemFactory factory=new DiskFileItemFactory();
+        ServletFileUpload sfu=new ServletFileUpload(factory);
+        sfu.setHeaderEncoding("UTF-8");  //处理中文问题
+        sfu.setSizeMax(1024*1024);   //限制文件大小
+        
+        try {
+            List<FileItem> fileItems= sfu.parseRequest(request);  //解码请求 得到所有表单元素
+            for (FileItem fi : fileItems) {
+                //有可能是 文件，也可能是普通文字 
+                if (fi.isFormField()) { //这个选项是 文字 
+                    System.out.println("表单值为："+fi.getString());
+                }else{
+                    // 是文件
+                	//获取图片后缀名
+                    String format=fi.getName().substring(fi.getName().indexOf("."), fi.getName().length());
+                    //图片命名
+                    fn=UUID.randomUUID().toString().replaceAll("-", "")+format;
+                    System.out.println("文件名是："+fn);  //文件名 
+                    // fn 是可能是这样的 c:\abc\de\tt\fish.jpg
+                    fi.write(new File(path,fn));
+                     
+                }                
+            }    
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }	}
+
+}
